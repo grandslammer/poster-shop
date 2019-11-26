2f728441bf2e1bec5eb8cff571515baf757356a1
Ivan White <ivanwhitemac@gmail.com>
2019-11-26 13:51:51 +0000

    Section 2.7

--- public/script.js
+++ public/script.js
@@ -3,14 +3,22 @@ new Vue({
     data: {
         total: 0,
         products: [
-            {title: "Product 1", id: 1},
-            {title: "Product 2", id: 2},
-            {title: "Product 3", id: 3}
-        ]
+            {title: "Product 1", id: 1, price: 9.99},
+            {title: "Product 2", id: 2, price: 9.99},
+            {title: "Product 3", id: 3, price: 9.99}
+        ],
+        cart: []
     },
     methods: {
-        addToCart: () => {
-            this.total += 9.99
+        addToCart: function (product) {
+            this.total += product.price;
+            console.log(product.id);
+            this.cart.push({
+                id: product.id,
+                title: product.title,
+                price: product.price,
+                qty: 1
+            });
         }
     }
 });
